eee7a9b0055d12377d9c60f4bec438fbc144206f
tonnyCAPTAIN <tonnycaptain7@gmail.com>
2022-11-15 13:01:32 +0300

    find the difference

---
+++ spotthediffence.py
@@ -0,0 +1,11 @@
+def spotdiff():
+    t = "foobar"
+    s = "barfoot"
+    if len(s) > len(t):
+        for i in s: 
+            if i not in t:
+                return i
+    else:
+        for i in t:
+            if i not in s:
+                return i
